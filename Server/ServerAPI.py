c891347de830113871dbeaa7a2da657f9d5202d4
Nikolas Burzynski <nburzyn1@binghamton.edu>
2020-02-08 21:26:03 -0500

    Authentification Function
    Authentification Function Finished

--- Server/ServerAPI.py
+++ Server/ServerAPI.py
@@ -4,20 +4,41 @@ def auth(package):
     file = open("auth.txt", "r+");
     data = file.read().replace("\n", " ").split(" ");
     print(data);
+    inputUserID = package.split(":")[0];
+    inputPassword = package.split(":")[1];
+    inputPassword = inputPassword[0:len(inputPassword)-1];
+    print(inputUserID);
+    print(inputPassword);
+    allIds = [];
+    allPwds = [];
+    for users in data:
+        allIds.append(users.split(",")[0]);
+        allPwds.append(users.split(",")[1]);
+    print(allIds);
+    print(allPwds);
+    for x in range(0, len(allIds)+1):
+        print(x);
+        if(inputUserID == allIds[x] and inputPassword == allPwds[x]):
+            print("Authentification Complete");
+            return True;
+
+    return False;
 
 def get(package):
     print("Still needs to be written");
-    
+
 def _set(package):
     print("Still needs to be written");
-    
+
 
 def parse(package):
     package = package[1:len(package)]
 #   print(package);
     keyWord = package.split(":")[0];
     if(keyWord == "auth"):
-        #package = package[len(keyWord), len(package)];
+        package = package[len(keyWord) + 1 : len(package)];
+        print(len(keyWord));
+        print(len(package));
         auth(package);
     elif(keyWord == "get"):
         get(package);
@@ -27,18 +48,11 @@ def parse(package):
         print("Invalid Keyword");
 
 
-while(True):
-    inputPackage = input("Simulate an incoming package ");
+inputPackage = input("Simulate an incoming package ");
 
 
-    if(inputPackage[0] == "{"):
-#       print("Valid input");
-        parse(inputPackage);
-    else:
-        print("Invalid input");
-
-
-
-
-
-
+if(inputPackage[0] == "{"):
+#   print("Valid input");
+    parse(inputPackage);
+else:
+    print("Invalid input");
