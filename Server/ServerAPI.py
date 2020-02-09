9eb02745a83f2e5e30e22996dde9c625e2444b88
Nikolas Burzynski <nburzyn1@binghamton.edu>
2020-02-09 08:43:19 -0500

    In and Out Photos
    Provides implementation of photo analysis to the server

--- Server/ServerAPI.py
+++ Server/ServerAPI.py
@@ -1,9 +1,14 @@
 import ServerSockets
+import cv2
+import os.path
+import add_image
+from os import path
+
 
 #def didPass():
 #    return "Allow"
 
-def auth(packet):
+def auth(packet, img = None):
     #print(packet)
     outPutPacket = packet
     packetLength = len(outPutPacket)
@@ -29,14 +34,14 @@ def auth(packet):
         #print(x)
         if(inputUserID == allIds[x] and inputPassword == allPwds[x]):
             print("Authentification Complete")
-            return postAuthParse(outPutPacket, inputUserID)
+            return postAuthParse(outPutPacket, inputUserID, img)
             break
         else:
             print("Invalid userID and/or userPWD")
-            return "Deny"
+            return "Deny", False
 
 
-def postAuthParse(packet, UserID):
+def postAuthParse(packet, UserID, img = None):
     #print(packet)
     splitPacket = packet.split(":")
     function = splitPacket[0]
@@ -66,17 +71,17 @@ def postAuthParse(packet, UserID):
                     profileInfo.seek(0)
                     profileInfo.write(profInfo[x])
                     profileInfo.close()
-                    return argument;
+                    return argument, False
                 elif(x == 0):
                     profileInfo.seek(0)
                     profileInfo.write(profInfo[x] + "\n")
                     profileInfo.close()
-                    return argument;
+                    return argument, False
                 else:
                     profileInfo.seek(0)
                     profileInfo.write(profInfo[x] + "\n")
                     profileInfo.close()
-                    return argument;
+                    return argument, False
                     #print("We done")
     elif(function == "get"):
         #print("Start Here")
@@ -87,28 +92,59 @@ def postAuthParse(packet, UserID):
             splitProfInfo = profInfo[x].split(",")
             if(splitProfInfo[0] == UserID):
                 if(argument == "First Name"):
-                    return splitProfInfo[1]
+                    return splitProfInfo[1], False
                 elif(argument == "Last Name"):
-                    return splitProfInfo[2]
+                    return splitProfInfo[2], False
                 elif(argument == "Status"):
-                    return splitProfInfo[3]
+                    return splitProfInfo[3], False
                 else:
-                    return "Invalid Argument"
+                    return "Invalid Argument", False
     elif(function == "login"):
-        return "Allow";
+        return "Allow", False
     elif(function == "getFirstName"):
         for x in range(0,len(profInfo)):
             splitInfo = profInfo[x].split(",")
             if(splitInfo[0] == argument):
-                return splitInfo[1]
+                return splitInfo[1], False
     elif(function == "getStatus"):
         for x in range(0,len(profInfo)):
             splitInfo = profInfo[x].split(",")
             if(splitInfo[0] == argument):
-                return splitInfo[3]
+                return splitInfo[3], False
+    elif(function[0] == "i"):
+        #photo mode activated
+        functionLength = len(function);
+        index = function[1:len(function)];
+        targetId = UserID;
+        directory = "Imgs/" + targetId + "/" + index + "vP.jpg"
+        if(path.exists(directory)):
+            img = cv2.imread(directory)
+            print("basic")
+            return ServerSockets.picture_to_data(img), True
+
+        directory = "Imgs/" + targetId + "/" + index + "vH.jpg"
+        if(path.exists(directory) and targetId == UserID):
+            print("Private")
+            img = cv2.imread(directory)
+            return ServerSockets.picture_to_data(img), True
+
+        print("No photo")
+        return "None", False
+    elif(function == "P" or function == "H"):
+        for x in range(1,100):
+            location = "Imgs/" + UserID + "/" + x + "vP.jpg"
+            if(path.exists(location)):
+                continue
+            location = "Imgs/" + UserID + "/" + x + "vH.jpg"
+            if(path.exists(location)):
+                continue
+            location = "Imgs/" + UserID + "/" + x + "v" + function + ".jpg"
+            img.imwrite(location)
+            add_image.add_img(location)
+            return str(x);
     else:
         print("Invalid function")
-        return "Invalid"
+        return "Invalid", False
 
 
 def createID(packet):
@@ -125,6 +161,8 @@ def createID(packet):
     userID = "00000" + str(numberUsers)
     while(len(userID) > 6):
         userID = userID[1:len(UserID)]
+    path = "Imgs/" + userID;
+    os.mkdir(path);
     splitPacket = packet.split(":")
     firstName = splitPacket[1]
     lastName = splitPacket[2]
@@ -136,15 +174,15 @@ def createID(packet):
         auth.write("\n" + userID + "," + pswd)
     if(currentProfiles == ""):
         profileInfo.write(userID + "," + firstName + "," + lastName + "," + "relation")
-        return userID
+        return userID, False
     else:
         profileInfo.write("\n" + userID + "," + firstName + "," + lastName + "," + "relation")
-        return userID
+        return userID, False
 
     auth.close()
     profileInfo.close()
 
-def parse(TXTdata):
+def parse(TXTdata, img = None):
     TXTdata = TXTdata[1:len(TXTdata)]
 #   print(packet)
     keyWord = TXTdata.split(":")[0]
@@ -152,7 +190,7 @@ def parse(TXTdata):
         TXTdata = TXTdata[len(keyWord) + 1 : len(TXTdata)]
         #print(len(keyWord))
         #print(len(packet))
-        return auth(TXTdata)
+        return auth(TXTdata, img)
     elif(keyWord == "createID"):
         return createID(TXTdata)
     else:
@@ -164,7 +202,7 @@ def recieveTXTPacket(data):
     return parse(data)
 
 def recieveIMGPacket(data, img):
-    parse(data,img)
+    parse(data,img = img)
 
 #Code Starts Here
 net = ServerSockets.SocketHandler(recieveTXTPacket,recieveIMGPacket)
