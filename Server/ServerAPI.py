65726d92166579eb2f1c93cb1e6467feac69645d
Nikolas Burzynski <nburzyn1@binghamton.edu>
2020-02-09 05:20:10 -0500

    Get/Set functionality
    Allow a packet to be parsed that changes the relation status of a profile in the database

--- Server/ServerAPI.py
+++ Server/ServerAPI.py
@@ -4,7 +4,7 @@ def didPass():
 
 
 def auth(package):
-    print(package)
+    #print(package)
     outPutPackage = package
     packageLength = len(outPutPackage)
     file = open("auth.txt", "r+")
@@ -12,7 +12,7 @@ def auth(package):
     file.close()
     #print(data)
     splitPackage = package.split(":")
-    print(splitPackage)
+    #print(splitPackage)
     inputUserID = splitPackage[0]
     inputPassword = splitPackage[1]
     #print(inputUserID)
@@ -32,36 +32,59 @@ def auth(package):
             didPass()
             postAuthParse(outPutPackage, inputUserID)
             break
-
-    print("Invalid userID and/or userPWD")
+        else:
+            print("Invalid userID and/or userPWD")
 
 
 def postAuthParse(package, UserID):
-    print(package)
+    #print(package)
+    splitPackage = package.split(":")
+    function = splitPackage[0]
+    argumentLength = len(splitPackage[1])
+    argument = splitPackage[1][0:argumentLength-1]
+    #print("USER ID")
+    #print(UserID)
     profileInfo = open("profileInfo.txt", "r+")
     profInfo = profileInfo.read().split("\n")
-    print(profInfo)
-    splitPackage = package.split(":")
-    packageFunction = splitPackage[0]
-    argLength = len(splitPackage[1])
-    arg = splitPackage[1][0:argLength-1]
-    if(packageFunction == "set"):
-        for x in range(0, len(profInfo)):
-            profileId = profInfo[x][0:6]
-            #print("CHECK HERE")
-            #print(profileId)
+    #print(profInfo)
+    if(function == "set"):
+        for x in range(0,len(profInfo)):
+            splitInfo = profInfo[x].split(",")
+            if(splitInfo[0] == UserID):
+            #print("Changing status")
+            #print(splitInfo[3])
+                tempStringArray = profInfo[x].split(",")
+                splitInfo[3] = argument
+                profInfo[x] = tempStringArray[0] + "," + tempStringArray[1] + "," + tempStringArray[2] + "," + splitInfo[3]
+            #print(profInfo[x])
+            #print(splitInfo[3])
+            #print("Done changing the array")
+            #print(profInfo)
+            profileInfo.truncate(0)
+            for x in range(0,len(profInfo)):
+                if(x == len(profInfo)-1):
+                    profileInfo.write(profInfo[x])
+                elif(x == 0):
+                    profileInfo.seek(0)
+                    profileInfo.write(profInfo[x] + "\n")
+                else:
+                    profileInfo.write(profInfo[x] + "\n")
+                    profileInfo.close();
+                    #print("We done")
+    elif(function == "get"):
+        #print("Start Here")
+        #print(profInfo)
+        for x in range(0,len(profInfo)):
+            #print(profInfo[x])
             #print(UserID)
-            if(UserID == profileId):
-                print("CHECK UNDER ME")
-                print(arg)
-                profInfo[x].split(",")[3] = arg
-                print(profInfo[x].split(",")[3])
-        return
-    elif(packageFunction == "get"):
-        return
-    elif(packageFunction == "analyze"):
-        return
-    #print(packageFunction)
+            splitProfInfo = profInfo[x].split(",")
+            if(splitProfInfo[0] == UserID):
+                print("Recieved Information")
+                print(splitProfInfo[3])
+
+    else:
+        print("Invalid function")
+
 
 def createID(package):
     auth = open("auth.txt", "r+")
@@ -86,9 +109,9 @@ def createID(package):
     else:
         auth.write("\n" + userID + "," + pswd)
     if(currentProfiles == ""):
-        profileInfo.write("{'UserID':" + userID + "," + "'FirstName':" +  firstName + "," + "'LastName':" + lastName + "," + "'Status':" + "relation" + "}")
+        profileInfo.write(userID + "," + firstName + "," + lastName + "," + "relation")
     else:
-        profileInfo.write("\n" "{'UserID':" + userID + "," + "'FirstName':" +  firstName + "," + "'LastName':" + lastName + "," + "'Status':" + "relation" + "}")
+        profileInfo.write("\n" + userID + "," + firstName + "," + lastName + "," + "relation")
 
     auth.close()
     profileInfo.close()
@@ -103,8 +126,8 @@ def parse(package):
     keyWord = package.split(":")[0]
     if(keyWord == "auth"):
         package = package[len(keyWord) + 1 : len(package)]
-        print(len(keyWord))
-        print(len(package))
+        #print(len(keyWord))
+        #print(len(package))
         auth(package)
     elif(keyWord == "createID"):
         createID(package)
