11d2c9c21522ebf9440c1c4f36d57d90bc340c42
Jacob Feinland <jujubuscus@gmail.com>
2020-02-09 11:50:37 -0500

    everything on server

--- Server/make_svm.py
+++ Server/make_svm.py
@@ -7,4 +7,4 @@ def makeSVM:
     
 def recog(path):
     os.system('cmd /k "python recognize.py --detector face_detection_model --embedding-model "' +
-              '"openface_nn4.small2.v1.t7 --recognizer output/recognizer.pickle --le output/le.pickle --image "' + path + '".jpg"')
+              '"openface_nn4.small2.v1.t7 --recognizer output/recognizer.pickle --le output/le.pickle --image "' + path)
