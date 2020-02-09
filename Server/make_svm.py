c57f297e96e2374313ab99924cbf1cfd650b4d21
Jacob Feinland <jujubuscus@gmail.com>
2020-02-09 11:42:13 -0500

    make svm and recog

--- Server/make_svm.py
+++ Server/make_svm.py
@@ -5,3 +5,6 @@ def makeSVM:
     os.system('cmd /k "python train_model.py --embeddings output/embeddings.pickle --recognizer "'+
                '"output/recognizer.pickle --le output/le.pickle"')
     
+def recog(path):
+    os.system('cmd /k "python recognize.py --detector face_detection_model --embedding-model "' +
+              '"openface_nn4.small2.v1.t7 --recognizer output/recognizer.pickle --le output/le.pickle --image Imgs/Analyze "' + path + '".jpg"')
