a15151f7a0315784a2bc58611e8d48b8b7598810
Jacob Feinland <jujubuscus@gmail.com>
2020-02-09 11:31:38 -0500

    update add_image

--- add_image.py
+++ add_image.py
@@ -1,5 +1,5 @@
 import os
 
-def add_img(path):
+def add_img():
     os.system('cmd /k "python extract_embeddings.py --dataset dataset"' + 
               '" --embeddings output/embeddings.pickle --detector face_detection_model --embedding-model openface_nn4.small2.v1.t7"')
