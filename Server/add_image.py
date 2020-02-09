4446116c02e7e99d488cbb44cc9321828a9c4c35
Jacob Feinland <jujubuscus@gmail.com>
2020-02-09 11:29:40 -0500

    change add_img

--- Server/add_image.py
+++ Server/add_image.py
@@ -1,5 +1,5 @@
 import os
 
 def add_img(path):
-    os.system('cmd /k "python extract_embeddings.py --dataset dataset"' + 
+    os.system('cmd /k "python extract_embeddings_initial.py --dataset dataset"' +
               '" --embeddings output/embeddings.pickle --detector face_detection_model --embedding-model openface_nn4.small2.v1.t7"')
